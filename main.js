82111a91fc558e008e5ff30606cc728ba9cd337c
Will Dodson <willdodson@Wills-Air.lan>
2023-03-16 10:30:46 -0400

    added js transitions and started media

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 // window.alert("Script is working")
 
 const displayTime = () => {
-    let now = new Date();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
 
-    minutes = (minutes < 10 ? "0" : "") + minutes;
-    seconds = (seconds < 10 ? "0" : "") + seconds;
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
 
-    let period = (hours < 12 ? "AM" : "PM");
-    
-    hours = (hours > 12) ? hours - 12 : hours;
-    hours = (hours === 0) ? 12 : hours;
+  let period = hours < 12 ? "AM" : "PM";
 
-    let clock = document.getElementById("time");
+  hours = hours > 12 ? hours - 12 : hours;
+  hours = hours === 0 ? 12 : hours;
 
-    clock.textContent = hours + ":" + minutes + " " + period;
+  let clock = document.getElementById("time");
 
+  clock.textContent = hours + ":" + minutes + " " + period;
 
-    setTimeout(displayTime, 1000)
+  setTimeout(displayTime, 1000);
+};
+
+displayTime();
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
 }
 
-displayTime()
+window.addEventListener("scroll", reveal);
