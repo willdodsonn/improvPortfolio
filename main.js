f856ae55102fd280333ad9fde164c7058be3638a
Will Dodson <willdodson@Wills-MacBook-Air.local>
2023-03-21 09:52:27 -0400

    why isnt this working

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// window.alert("Script is working")
+
+const displayTime = () => {
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+
+  let period = hours < 12 ? "AM" : "PM";
+
+  hours = hours > 12 ? hours - 12 : hours;
+  hours = hours === 0 ? 12 : hours;
+
+  let clock = document.getElementById("time");
+
+
+  setTimeout(displayTime, 1000);
+};
+
+displayTime();
 
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
